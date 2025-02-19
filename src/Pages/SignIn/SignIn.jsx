79e2560cc4afebd2d9eb0b222b7618c14b81475c
Chitra35006 <ch18.cse@gmail.com>
2025-02-19 12:08:39 +0600

    adding darkmode to login & registration page

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -9,14 +9,17 @@ import DynamicButton from "../Buttons/DynamicButton";
 import { IoLogoGoogle } from "react-icons/io5";
 import "animate.css";
 import logo from "../../assets/logo.png";
-import { useTheme } from "../../Provider/ThemeContext";
+
 import useAuth from "../../hooks/useAuth";
 import Swal from "sweetalert2";
 import { MdError } from "react-icons/md";
 import SocioLogin from "../SocioLogin/SocioLogin";
-import {ArrowRightOutlined } from "@ant-design/icons";
+import { ArrowRightOutlined } from "@ant-design/icons";
+import useTheme from "../../hooks/useTheme";
+
 const SignIn = () => {
-  const{signIn} = useAuth();
+  const { signIn } = useAuth();
+  const { theme } = useTheme();
   const {
     register,
     handleSubmit,
@@ -28,74 +31,76 @@ const SignIn = () => {
   const location = useLocation();
   const from = location.state?.from?.pathname || "/";
   const [errorMessage, setErrorMessage] = useState("");
-  //handleSubmit
-  const onSubmit = (data) => {
-    console.log(data);
-    signIn(data.email,data.password)
-    .then(result =>{
-      const user = result.user;
-      console.log(user);
-      Swal.fire({
-        title: 'User Login Successful.',
-        showClass: {
-            popup: 'animate__animated animate__fadeInDown'
-        },
-        hideClass: {
-            popup: 'animate__animated animate__fadeOutUp'
-        }
-    });
-    navigate(from, { replace: true });
-    })
-    .catch((error) => {
-      console.error(error);
-      if (error.message.includes("user-not-found")) {
-        setErrorMessage("This email is not registered. Please sign up first.");
-      } else if (error.message.includes("wrong-password")) {
-        setErrorMessage("The password is incorrect. Please try again.");
-      } else {
-        setErrorMessage("An unexpected error occurred. Please try again.");
-      }
-    });
 
+  // Handle form submission
+  const onSubmit = (data) => {
+    signIn(data.email, data.password)
+      .then((result) => {
+        Swal.fire({
+          title: "User Login Successful.",
+          showClass: {
+            popup: "animate__animated animate__fadeInDown",
+          },
+          hideClass: {
+            popup: "animate__animated animate__fadeOutUp",
+          },
+        });
+        navigate(from, { replace: true });
+      })
+      .catch((error) => {
+        if (error.message.includes("user-not-found")) {
+          setErrorMessage("This email is not registered. Please sign up first.");
+        } else if (error.message.includes("wrong-password")) {
+          setErrorMessage("The password is incorrect. Please try again.");
+        } else {
+          setErrorMessage("An unexpected error occurred. Please try again.");
+        }
+      });
   };
-  const handleInputChange = () => {
-    setErrorMessage(""); // Clear error message when user starts typing
-  };
-  const { theme } = useTheme();
+
   return (
-    <div className="p-10 bg-[linear-gradient(25deg,#99f6e4_5%,_white_30%,_white_70%,#d9f99d_100%)]">
+    <div
+      className={`p-10 ${
+        theme === "dark"
+          ? "bg-slate-950"
+          : "bg-[linear-gradient(25deg,#99f6e4_5%,_white_30%,_white_70%,#d9f99d_100%)]"
+      }`}
+    >
       <Helmet>
         <title>Perfect Pair | Login</title>
       </Helmet>
+
       <div>
-      <a
-    href="/"
-    className="flex items-center text-lime-500 hover:text-blue-500 transition-colors duration-300"
-  >
-    <ArrowRightOutlined className="text-xl md:text-2xl" />
-    <span className="ml-1 text-sm md:text-base font-medium">Go Home</span>
-  </a>
-      </div>
-      <div className="text-xl font-bold flex my-4 justify-center items-center">
         <a
           href="/"
-          className="text-gray-800 flex items-center md:text-2xl text-xl"
+          className="flex items-center text-lime-500 hover:text-blue-500 transition-colors duration-300"
         >
-          <img className="w-10 mr-2 animate-pulse" src={logo} alt="Logo" />{" "}
+          <ArrowRightOutlined className="text-xl md:text-2xl" />
+          <span className="ml-1 text-sm md:text-base font-medium">Go Home</span>
+        </a>
+      </div>
+
+      <div className="text-xl font-bold flex my-4 justify-center items-center">
+        <a href="/" className="text-gray-800 flex items-center md:text-2xl text-xl">
+          <img className="w-10 mr-2 animate-pulse" src={logo} alt="Logo" />
           <h1
             className={`${
               theme === "dark" ? "text-gray-200" : "text-black"
             } animate__animated animate__bounce my-2`}
-            style={{
-              animationIterationCount: 2, // Run the animation only once
-            }}
           >
             <span className="text-lime-500">P</span>erfect{" "}
             <span className="text-lime-500">P</span>air
           </h1>
         </a>
       </div>
-      <div className="grid grid-cols-12 md:flex-col flex-row min-h-screen bg-[linear-gradient(25deg,#bef264_5%,_white_30%,_white_70%,#99f6e4_100%)] rounded-xl shadow-md">
+
+      <div
+        className={`grid grid-cols-12 md:flex-col flex-row min-h-screen rounded-xl shadow-md ${
+          theme === "dark"
+            ? "bg-[linear-gradient(25deg,#3f6212_5%,_#1f2937_30%,_#1f2937_70%,_#065f46_100%)]"
+            : "bg-[linear-gradient(25deg,#bef264_5%,_white_30%,_white_70%,#99f6e4_100%)]"
+        }`}
+      >
         {/* Form Section */}
         <div className="md:col-span-6 col-span-12 order-2 md:order-1 border-lime-400 border-dashed border-l-4 flex flex-col items-center justify-center p-6">
           {/* Form */}
@@ -104,46 +109,41 @@ const SignIn = () => {
               Login Now
             </h2>
           </div>
+
           <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-md">
             {/* Email */}
             <div className="form-control w-full my-4">
               <TextField
                 type="email"
-                label="Email"
+                label={<span className={theme === "dark" ? "text-gray-300" : ""}>Email</span>}
                 variant="outlined"
                 fullWidth
                 {...register("email", { required: true })}
                 required
-                onInput={handleInputChange} 
               />
-              {errors.email && (
-                <span className="text-red-600">Email is required</span>
-              )}
+              {errors.email && <span className="text-red-600">Email is required</span>}
             </div>
-            
 
             {/* Password */}
             <div className="form-control w-full my-4">
               <TextField
                 type="password"
-                label="Password"
+                label={<span className={theme === "dark" ? "text-gray-300" : ""}>Password</span>}
                 variant="outlined"
                 fullWidth
                 {...register("password", { required: true })}
                 required
-                onInput={handleInputChange} 
               />
-              {errors.password && (
-                <span className="text-red-600">Password is required</span>
-              )}
+              {errors.password && <span className="text-red-600">Password is required</span>}
             </div>
+
             {/* Error Message */}
-                         {errorMessage && (
-                          <p className="text-red-600 mt-2 flex items-center">
-                            <MdError className="text-yellow-400 bg-red-600 rounded-full mr-1" />
-                            {errorMessage}
-                          </p>
-                        )}
+            {errorMessage && (
+              <p className="text-red-600 mt-2 flex items-center">
+                <MdError className="text-yellow-400 bg-red-600 rounded-full mr-1" />
+                {errorMessage}
+              </p>
+            )}
 
             <p className="text-center text-gray-500 font-semibold">
               Don't have an account?{" "}
@@ -153,19 +153,19 @@ const SignIn = () => {
             </p>
 
             <div className="mt-6 flex justify-center items-center gap-4">
-              <DynamicButton color="green" text={"SIGN IN"}></DynamicButton>
+              <DynamicButton color="green" text={"SIGN IN"} />
             </div>
           </form>
 
           {/* Google Button */}
-          <SocioLogin></SocioLogin>
+          <SocioLogin />
         </div>
 
         {/* Lottie React Section */}
         <div className="md:col-span-6 col-span-12 order-1 md:order-2 flex items-center justify-center border-cyan-400 border-dashed border-r-4 md:border-b-0 border-b-4">
-          {/* Add Lottie Animation or Placeholder */}
+          {/* Lottie Animation */}
           <div className="w-2/3 h-2/3 flex items-center justify-center">
-            <Lottie animationData={regAnimation}></Lottie>
+            <Lottie animationData={regAnimation} />
           </div>
         </div>
       </div>
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,9 +1,9 @@
-import { TextField, useTheme } from "@mui/material";
-import React, { useContext, useState } from "react";
+import { TextField } from "@mui/material";
+import React, { useState } from "react";
 import { Helmet } from "react-helmet";
 import { useForm } from "react-hook-form";
 import { MdError } from "react-icons/md";
-import { Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Lottie from "lottie-react";
 import regAnimation from "../../assets/lottie.json";
 import DynamicButton from "../Buttons/DynamicButton";
@@ -12,175 +12,151 @@ import logo from "../../assets/logo.png";
 import useAuth from "../../hooks/useAuth";
 import Swal from "sweetalert2";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
-import {ArrowRightOutlined } from "@ant-design/icons";
+import { ArrowRightOutlined } from "@ant-design/icons";
+import useTheme from "../../hooks/useTheme";
+
 const SignUp = () => {
-  const{createUser,updateUserProfile} = useAuth();
+  const { createUser, updateUserProfile } = useAuth();
+  const { theme } = useTheme();
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+  const axiosPublic = useAxiosPublic();
+
   const {
     register,
     handleSubmit,
     reset,
     formState: { errors },
   } = useForm();
-  const { theme } = useTheme();
-  const [errorMessage, setErrorMessage] = useState("");
- const navigate = useNavigate();
 
- const axiosPublic = useAxiosPublic();
-  //handleSubmit
- 
- 
-
+  // Handle form submission
   const onSubmit = (data) => {
-    console.log(data);
     createUser(data.email, data.password)
-    .then(result => {
-      const logggedUser = result.user;
-      console.log(logggedUser);
-      updateUserProfile(data.name, data.photoURL)
-      .then(()=>{
+      .then((result) => {
+        updateUserProfile(data.name, data.photoURL).then(() => {
+          const userInfo = {
+            name: data.name,
+            email: data.email,
+            photo: data.photoURL,
+          };
 
-        const userInfo={
-          name: data.name,
-          email:data.email,
-          photo:data.photoURL
-          
-        }
-        axiosPublic.post('/users',userInfo)
-        .then(res =>{
-          if(res.data.insertedId){
-            reset();
-        Swal.fire({
-          position: 'top-end',
-          icon: 'success',
-          title: 'User created successfully.',
-          showConfirmButton: false,
-          timer: 1500
-      });
-      navigate('/');
-          }
-        })
-        
-     
-      
-    })
-    .then(error => console.log(error))
+          axiosPublic.post("/users", userInfo).then((res) => {
+            if (res.data.insertedId) {
+              reset();
+              Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "User created successfully.",
+                showConfirmButton: false,
+                timer: 1500,
+              });
+              navigate("/");
+            }
+          });
+        });
       })
-      
-    .catch((error) => {
-      console.error(error);
-      if (error.message.includes("email-already-in-use")) {
-        setErrorMessage("The email address is already in use.");
-      } else {
-        setErrorMessage("An unexpected error occurred. Please try again.");
-      }
-    });
+      .catch((error) => {
+        console.error(error);
+        if (error.message.includes("email-already-in-use")) {
+          setErrorMessage("The email address is already in use.");
+        } else {
+          setErrorMessage("An unexpected error occurred. Please try again.");
+        }
+      });
   };
+
   return (
-    <div className="p-10 bg-[linear-gradient(25deg,#99f6e4_5%,_white_30%,_white_70%,#d9f99d_100%)]">
+    <div
+      className={`p-10 ${
+        theme === "dark"
+          ? "bg-slate-950"
+          : "bg-[linear-gradient(25deg,#99f6e4_5%,_white_30%,_white_70%,#d9f99d_100%)]"
+      }`}
+    >
       <Helmet>
         <title>Perfect Pair | Register</title>
       </Helmet>
       <div className="text-xl font-bold flex my-4 justify-center items-center">
-        <a
-          href="/"
-          className="text-gray-800 flex items-center md:text-2xl text-xl"
-        >
+        <a href="/" className="text-gray-800 flex items-center md:text-2xl text-xl">
           <img className="w-10 mr-2 animate-pulse" src={logo} alt="Logo" />{" "}
           <h1
             className={`${
               theme === "dark" ? "text-gray-200" : "text-black"
             } animate__animated animate__bounce my-2`}
-            style={{
-              animationIterationCount: 2, // Run the animation only once
-            }}
           >
             <span className="text-lime-500">P</span>erfect{" "}
             <span className="text-lime-500">P</span>air
           </h1>
         </a>
       </div>
-      <div className="grid grid-cols-12 md:flex-col flex-row min-h-screen bg-[linear-gradient(25deg,#bef264_5%,_white_30%,_white_70%,#99f6e4_100%)] rounded-xl shadow-md">
+
+      <div
+        className={`grid grid-cols-12 md:flex-col flex-row min-h-screen rounded-xl shadow-md ${
+          theme === "dark"
+            ? "bg-[linear-gradient(25deg,#3f6212_5%,_#1f2937_30%,_#1f2937_70%,_#065f46_100%)]"
+            : "bg-[linear-gradient(25deg,#bef264_5%,_white_30%,_white_70%,#99f6e4_100%)]"
+        }`}
+      >
         {/* Form Section */}
-        <div className="md:col-span-6 col-span-12 order-2 md:order-1 border-lime-400 border-dashed border-l-4 flex  items-center justify-center p-6 relative">
-          {/* Form */}
+        <div className="md:col-span-6 col-span-12 order-2 md:order-1 border-lime-400 border-dashed border-l-4 flex items-center justify-center p-6 relative">
           <div className="flex absolute top-4 md:top-5 justify-between items-center w-full px-4">
- 
+            {/* Arrow Link Section */}
+            <a
+              href="/"
+              className="flex items-center text-lime-500 hover:text-blue-500 transition-colors duration-300"
+            >
+              <ArrowRightOutlined className="text-xl md:text-2xl" />
+              <span className="ml-1 text-sm md:text-base font-medium">Go Home</span>
+            </a>
 
-  {/* Arrow Link Section */}
-  <a
-    href="/"
-    className="flex items-center text-lime-500 hover:text-blue-500 transition-colors duration-300"
-  >
-    <ArrowRightOutlined className="text-xl md:text-2xl" />
-    <span className="ml-1 text-sm md:text-base font-medium">Go Home</span>
-  </a>
-   {/* Heading Section */}
-   <h2 className="font-bold md:text-2xl text-lime-600 p-1 border-y-2 border-blue-300 px-2">
-    Register Now
-  </h2>
-</div>
+            {/* Heading Section */}
+            <h2 className="font-bold md:text-2xl text-lime-600 p-1 border-y-2 border-blue-300 px-2">
+              Register Now
+            </h2>
+          </div>
 
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="w-full max-w-md md:my-0 my-8"
-          >
+          <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-md md:my-0 my-8">
             {/* Name */}
             <div className="form-control w-full my-4">
-              <TextField
-                type="text"
-                label="Name"
-                variant="outlined"
-                fullWidth
-                {...register("name", { required: true })}
-                required
-              />
-              {errors.name && (
-                <span className="text-red-600">Name is required</span>
-              )}
-            </div>
+  <TextField
+    type="text"
+    label={<span className={theme === "dark" ? "text-gray-300" : ""}>Name</span>}
+    variant="outlined"
+    fullWidth
+    {...register("name", { required: "Name is required" })}
+    required
+  />
+  {errors.name && <span className="text-red-600">{errors.name.message}</span>}
+</div>
+
 
             {/* Email */}
             <div className="form-control w-full my-4">
               <TextField
                 type="email"
-                label="Email"
+                label={<span className={theme === "dark" ? "text-gray-300" : ""}>Email</span>}
                 variant="outlined"
                 fullWidth
                 {...register("email", { required: true })}
                 required
+                
               />
-              {errors.email && (
-                <span className="text-red-600">Email is required</span>
-              )}
+              {errors.email && <span className="text-red-600">Email is required</span>}
             </div>
-             {/* Error Message */}
-             {errorMessage && (
+
+            {/* Error Message */}
+            {errorMessage && (
               <p className="text-red-600 mt-2 flex items-center">
                 <MdError className="text-yellow-400 bg-red-600 rounded-full mr-1" />
                 {errorMessage}
               </p>
             )}
 
-            {/* Photo */}
-            <div className="form-control w-full my-4">
-              <TextField
-                type="text"
-                label="Photo"
-                variant="outlined"
-                fullWidth
-                {...register("photoURL", { required: true })}
-                required
-              />
-              {errors.photoURL && (
-                <span className="text-red-600">Photo URL is required</span>
-              )}
-            </div>
-
             {/* Password */}
             <div className="form-control w-full my-4">
               <TextField
                 type="password"
-                label="Password"
+                label={<span className={theme === "dark" ? "text-gray-300" : ""}>Password</span>}
                 variant="outlined"
                 fullWidth
                 {...register("password", {
@@ -191,26 +167,10 @@ const SignUp = () => {
                 })}
                 required
               />
-              {errors.password?.type === "required" && (
-                <p className="text-red-600 mt-1 flex items-center"><MdError className="text-yellow-400 bg-red-600 rounded-full" />Password is required</p>
-              )}
-              {errors.password?.type === "minLength" && (
-                <p className="text-red-600 mt-1 flex items-center"><MdError className="text-red-600 w-8 h-8  rounded-full" />Password must be 6 characters</p>
-              )}
-              {errors.password?.type === "maxLength" && (
-                <p className="text-red-600 mt-1 flex items-center"><MdError className="text-red-600 w-8 h-8  rounded-full" />
-                  Password must be less than 20 characters
-                </p>
-              )}
-              {errors.password?.type === "pattern" && (
-                <p className="text-red-600 mt-1 flex items-center"><MdError className="text-red-600 w-8 h-8  rounded-full" />
-                  Password must have one Uppercase one lower case, one number
-                  and one special character.
-                </p>
-              )}
             </div>
 
-            <p className="text-center text-gray-500 font-semibold">
+            {/* Already Registered Section */}
+            <p className={`text-center font-semibold ${theme === "dark" ? "text-lime-300" : "text-gray-500"}`}>
               Already Registered? Go to{" "}
               <Link className="md:ml-2 hover:text-lime-500 text-indigo-900" to="/login">
                 Login
@@ -225,8 +185,7 @@ const SignUp = () => {
 
         {/* Lottie React Section */}
         <div className="md:col-span-6 col-span-12 order-1 md:order-2 flex items-center justify-center border-cyan-400 border-dashed border-r-4 md:border-b-0 border-b-4">
-          {/* Add Lottie Animation or Placeholder */}
-          <div className="w-2/3 h-2/3  flex items-center justify-center">
+          <div className="w-2/3 h-2/3 flex items-center justify-center">
             <Lottie animationData={regAnimation}></Lottie>
           </div>
         </div>
@@ -234,7 +193,5 @@ const SignUp = () => {
     </div>
   );
 };
-{
-  /* <FaUtensils className="ml-4"></FaUtensils> */
-}
+
 export default SignUp;
